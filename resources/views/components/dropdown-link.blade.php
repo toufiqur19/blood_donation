235f20c04391a4f1d7a072cd24efd25ee4176a37
toufiqur19 <graphicsnet9@gmail.com>
2024-02-14 23:22:43 +0600

    create project and configure home/admin navbar

---
+++ resources/views/components/dropdown-link.blade.php
@@ -0,0 +1,1 @@
+<a {{ $attributes->merge(['class' => 'block py-1 mt-2 ml-4 w-full text-start text-md leading-5 text-gray-700 focus:outline-none focus:bg-gray-100']) }}>{{ $slot }}</a>
---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,4 @@
+<x-app-layout>
+    
+    <h1>home page</h1>
+</x-app-layout>
